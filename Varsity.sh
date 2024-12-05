c1344b4985500e4a171d3d4e12eee718a0d6694a
Niher Ronjon Pramanik <130027941+niherronjon@users.noreply.github.com>
2024-12-05 10:56:04 +0600

    Update Varsity.sh

--- Varsity.sh
+++ Varsity.sh
@@ -604,3 +604,312 @@ for (i = 1; i <= 5; i++) {
 #Run
 #./a.out
 #Output
+
+
+
+
+
+
+
+
+//Shortest Job First (SJF)
+
+
+
+// New File create : touch sjf.c
+//Open IDE : vi sjf.c
+// Run Code : gcc sjf.c -o test3
+// Run Code : ./test3
+
+#include <stdio.h>
+
+int main()
+{
+    int n;
+    printf("Enter number of processes: ");
+    scanf("%d", &n);
+
+    int processes[n], burstTime[n], waitingTime[n], turnaroundTime[n];
+    int totalWaitingTime = 0, totalTurnaroundTime = 0;
+
+    for (int i = 0; i < n; i++) {
+        processes[i] = i + 1;
+        printf("Enter burst time for process %d: ", i + 1);
+        scanf("%d", &burstTime[i]);
+    }
+    // Sorting burst times to implement SJF (non-preemptive)
+    for (int i = 0; i < n - 1; i++) {
+        for (int j = i + 1; j < n; j++) {
+            if (burstTime[i] > burstTime[j]) {
+                int temp = burstTime[i];
+                burstTime[i] = burstTime[j];
+                burstTime[j] = temp;
+
+                temp = processes[i];
+                processes[i] = processes[j];
+                processes[j] = temp;
+            }
+        }
+    }
+
+    waitingTime[0] = 0; // First process has no waiting time
+    for (int i = 1; i < n; i++) {
+        waitingTime[i] = waitingTime[i - 1] + burstTime[i - 1];
+    }
+
+    for (int i = 0; i < n; i++) {
+        turnaroundTime[i] = waitingTime[i] + burstTime[i];
+    }
+
+    printf("Process\tBurst Time\tWaiting Time\tTurnaround Time\n");
+    for (int i = 0; i < n; i++) {
+        printf("%d\t%d\t\t%d\t\t%d\n", processes[i], burstTime[i], waitingTime[i], turnaroundTime[i]);
+        totalWaitingTime += waitingTime[i];
+        totalTurnaroundTime += turnaroundTime[i];
+    }
+    printf("\nAverage Waiting Time: %.2f\n", (float)totalWaitingTime / n);
+    printf("Average Turnaround Time: %.2f\n", (float)totalTurnaroundTime / n);
+    return 0;
+}
+
+
+/*
+Output:
+
+Enter number of processes: 3
+Enter burst time for process 1: 6
+Enter burst time for process 2: 8
+Enter burst time for process 3: 7
+
+*/
+
+
+
+
+
+
+//Round Robin
+
+
+// New File create : touch round_robin.c
+//Open IDE : vi round_robin
+// Run Code : gcc round_robin.c -o test1
+// Run Code : ./test1
+
+#include <stdio.h>
+
+int main()
+{
+    int n, quantum, t = 0, done = 0;
+    printf("Enter number of processes: ");
+    scanf("%d", &n);
+    int processes[n], burstTime[n], remaining[n], waitingTime[n], turnaroundTime[n];
+
+    for (int i = 0; i < n; i++)
+    {
+        processes[i] = i + 1;
+        printf("Enter burst time for process %d: ", i + 1);
+        scanf("%d", &burstTime[i]);
+        remaining[i] = burstTime[i];
+    }
+
+    printf("Enter time quantum: ");
+    scanf("%d", &quantum);
+
+    while (!done)
+    {
+        done = 1;
+        for (int i = 0; i < n; i++)
+        {
+            if (remaining[i] > 0)
+            {
+                done = 0;
+                if (remaining[i] > quantum)
+                {
+                    t += quantum;
+                    remaining[i] -= quantum;
+                } else
+                {
+                    t += remaining[i];
+                    waitingTime[i] = t - burstTime[i];
+                    remaining[i] = 0;
+                }
+            }
+        }
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+        turnaroundTime[i] = burstTime[i] + waitingTime[i];
+    }
+    printf("Process\tBurst Time\tWaiting Time\tTurnaround Time\n");
+    for (int i = 0; i < n; i++)
+    {
+        printf("%d\t%d\t\t%d\t\t%d\n", processes[i], burstTime[i], waitingTime[i], turnaroundTime[i]);
+    }
+    return 0;
+}
+
+
+/* Output :
+
+Enter number of processes: 3
+Enter burst time for process 1: 10
+Enter burst time for process 2: 5
+Enter burst time for process 3: 8
+Enter time quantum: 3
+
+*/
+
+
+
+
+
+
+
+
+
+//Priority scheduling
+
+
+// New File create : priority_scheduling.c
+//Open IDE : vi priority_scheduling.
+// Run Code : gcc priority_scheduling.c -o test2
+// Run Code : ./test2
+
+
+#include <stdio.h>
+
+int main() {
+    int n;
+    printf("Enter number of processes: ");
+    scanf("%d", &n);
+
+    int processes[n], burstTime[n], priority[n], waitingTime[n], turnaroundTime[n];
+    int totalWaitingTime = 0, totalTurnaroundTime = 0;
+
+    for (int i = 0; i < n; i++) {
+        processes[i] = i + 1;
+        printf("Enter burst time for process %d: ", i + 1);
+        scanf("%d", &burstTime[i]);
+        printf("Enter priority for process %d: ", i + 1);
+        scanf("%d", &priority[i]);
+    }
+
+    for (int i = 0; i < n - 1; i++) {
+        for (int j = i + 1; j < n; j++) {
+            if (priority[i] > priority[j]) {
+                int temp = priority[i];
+                priority[i] = priority[j];
+                priority[j] = temp;
+
+                temp = burstTime[i];
+                burstTime[i] = burstTime[j];
+                burstTime[j] = temp;
+
+                temp = processes[i];
+                processes[i] = processes[j];
+                processes[j] = temp;
+            }
+        }
+    }
+
+    waitingTime[0] = 0;
+    for (int i = 1; i < n; i++) {
+        waitingTime[i] = waitingTime[i - 1] + burstTime[i - 1];
+    }
+
+    for (int i = 0; i < n; i++) {
+        turnaroundTime[i] = waitingTime[i] + burstTime[i];
+    }
+
+    printf("Process\tBurst Time\tPriority\tWaiting Time\tTurnaround Time\n");
+    for (int i = 0; i < n; i++) {
+        printf("%d\t%d\t\t%d\t\t%d\t\t%d\n", processes[i], burstTime[i], priority[i], waitingTime[i], turnaroundTime[i]);
+        totalWaitingTime += waitingTime[i];
+        totalTurnaroundTime += turnaroundTime[i];
+    }
+
+    printf("\nAverage Waiting Time: %.2f\n", (float)totalWaitingTime / n);
+    printf("Average Turnaround Time: %.2f\n", (float)totalTurnaroundTime / n);
+
+    return 0;
+}
+
+
+/* Output :
+
+Enter number of processes: 3
+Enter burst time for process 1: 10
+Enter priority for process 1: 2
+Enter burst time for process 2: 5
+Enter priority for process 2: 1
+Enter burst time for process 3: 8
+Enter priority for process 3: 3
+
+
+*/
+
+
+
+
+
+
+
+//First Come First Serve (FCFS)
+
+
+// New File create : touch fcfs.c
+//Open IDE : vi fcfs.c
+// Run Code : gcc fcfs.c -o test4
+// Run Code : ./test4
+
+
+#include <stdio.h>
+
+int main() {
+    int n;
+    printf("Enter number of processes: ");
+    scanf("%d", &n);
+
+    int processes[n], burstTime[n], waitingTime[n], turnaroundTime[n];
+    int totalWaitingTime = 0, totalTurnaroundTime = 0;
+
+    for (int i = 0; i < n; i++) {
+        processes[i] = i + 1;
+        printf("Enter burst time for process %d: ", i + 1);
+        scanf("%d", &burstTime[i]);
+    }
+
+    waitingTime[0] = 0;
+    for (int i = 1; i < n; i++) {
+        waitingTime[i] = waitingTime[i - 1] + burstTime[i - 1];
+    }
+
+    for (int i = 0; i < n; i++) {
+        turnaroundTime[i] = waitingTime[i] + burstTime[i];
+    }
+
+    printf("Process\tBurst Time\tWaiting Time\tTurnaround Time\n");
+    for (int i = 0; i < n; i++) {
+        printf("%d\t%d\t\t%d\t\t%d\n", processes[i], burstTime[i], waitingTime[i], turnaroundTime[i]);
+        totalWaitingTime += waitingTime[i];
+        totalTurnaroundTime += turnaroundTime[i];
+    }
+
+    printf("\nAverage Waiting Time: %.2f\n", (float)totalWaitingTime / n);
+    printf("Average Turnaround Time: %.2f\n", (float)totalTurnaroundTime / n);
+
+    return 0;
+}
+
+
+/* Output :
+
+Enter number of processes: 4
+Enter burst time for process 1: 5
+Enter burst time for process 2: 9
+Enter burst time for process 3: 6
+Enter burst time for process 4: 3
+
+*/
